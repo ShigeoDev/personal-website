782dfad7e10c48e3d7c3d8ed2af323b6252bdb0b
ShigeoDev <84653162+ShigeoDev@users.noreply.github.com>
2024-07-24 14:45:33 -0700

    first push

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Nigel Loh',
+    template: '%s | Nigel Loh',
   },
-  description: 'This is my portfolio.',
+  description: 'Portfolio',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Nigel Loh',
+    description: 'Portfolio',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Nigel Loh Portfolio',
     locale: 'en_US',
     type: 'website',
   },
